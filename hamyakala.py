13183d60561e661a04cda5141c4113ae5fa2efd9
R.B. <ramazanbasaran34@gmail.com>
2026-09-01 13:08:50 +0300

    Kuralkan servis sayfasi

--- hamyakala.py
+++ hamyakala.py
@@ -199,7 +199,7 @@ HEDEFLER: dict[str, tuple[str, str]] = {
     # Bajaj/Kanuni (Kuralkan): 98 satış beklenirken 87 geliyor
     "kuralkan-satis-hepsi": ("https://www.ekuralkan.com/motosiklet-satis-noktalari", "html"),
     "kuralkan-satis-34":    ("https://www.ekuralkan.com/motosiklet-satis-noktalari?city=34", "html"),
-    "kuralkan-satis-6":     ("https://www.ekuralkan.com/motosiklet-satis-noktalari?city=6", "html"),
+    "kuralkan-servis":      ("https://www.ekuralkan.com/motosiklet-teslimat-noktalari", "html"),
 
     # --- spormoto ---
     "ktm-servis":       ("https://spormoto.com/ktm/ktm-servisler/", "html"),
